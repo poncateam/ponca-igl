a50b0405c4312a013029d2de2cea698b626b2c99
fauberva <florian.auberval@irit.fr>
2025-06-12 14:03:32 +0200

    [main] Loading the base mesh using igl

--- src/main.cpp
+++ src/main.cpp
@@ -4,38 +4,50 @@ This Source Code Form is subject to the terms of the Mozilla Public
  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-#include "main.h"
+#include <igl/opengl/glfw/Viewer.h>
+#include <iostream>
+#include <chrono>
+#include <igl/readOBJ.h>
+#include <igl/per_vertex_normals.h>
+
+/// Convenience function measuring and printing the processing time of F
+template <typename Functor>
+void measureTime( const std::string &actionName, Functor F ){
+    using namespace std::literals; // enables the usage of 24h instead of e.g. std::chrono::hours(24)
+
+    const std::chrono::time_point<std::chrono::steady_clock> start =
+            std::chrono::steady_clock::now();
+    F(); // run process
+    const auto end = std::chrono::steady_clock::now();
+    std::cout << actionName << " in " << (end - start) / 1ms << "ms.\n";
+}
+
+Eigen::MatrixXd cloudV, cloudN;
+Eigen::MatrixXi meshF;
 
 int main(int argc, char *argv[])
 {
- // Inline mesh of a cube
- const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-   0.0,0.0,0.0,
-   0.0,0.0,1.0,
-   0.0,1.0,0.0,
-   0.0,1.0,1.0,
-   1.0,0.0,0.0,
-   1.0,0.0,1.0,
-   1.0,1.0,0.0,
-   1.0,1.0,1.0).finished();
- const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-   0,6,4,
-   0,2,6,
-   0,3,2,
-   0,1,3,
-   2,7,6,
-   2,3,7,
-   4,6,7,
-   4,7,5,
-   0,4,5,
-   0,5,1,
-   1,5,7,
-   1,7,3).finished();
+    measureTime( "[libIGL] Load Demo Mesh", []()
+    {
+        std::string filename = "../assets/bunny.obj"; // Works if build was made inside the working directory, this will need to be included inside the build
 
- // Plot the mesh
- igl::opengl::glfw::Viewer viewer;
- viewer.data().set_mesh(V, F);
- viewer.data().set_face_based(true);
- viewer.launch();
+        igl::readOBJ(filename, cloudV, meshF);
+        igl::per_vertex_normals(cloudV, meshF, cloudN);
+    } );
+
+
+    // Check if normals have been properly loaded
+    int nbUnitNormal = cloudN.rowwise().squaredNorm().sum();
+    if ( nbUnitNormal != cloudV.rows() ) {
+        std::cerr << "[libIGL] An error occurred when computing the normal vectors from the mesh. Aborting..."
+                  << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Plot the mesh
+    igl::opengl::glfw::Viewer viewer;
+    viewer.data().set_mesh(cloudV, meshF);
+    viewer.data().set_face_based(true);
+    viewer.launch();
 }
 
